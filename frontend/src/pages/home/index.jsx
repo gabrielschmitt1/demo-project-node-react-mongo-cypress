704bc55c04bfcc5c0c12cfd3f85b6160577fa9f2
gabrielschmitt1 <gabrielschmitt7@gmail.com>
2024-06-03 22:09:56 -0300

    add edit feature

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,23 +1,24 @@
 import { useEffect, useState, useRef } from 'react'
 import './style.css'
 import Trash from '../../assets/trash.svg'
+import Edit from '../../assets/edit.svg'
 import api from '../../services/api'
 
 function Home() {
-  
+
   const [users, setUsers] = useState([])
+  const [editingUserId, setEditingUserId] = useState(null)
   const inputName = useRef()
   const inputEmail = useRef()
   const inputAge = useRef()
   const inputAddress = useRef()
 
-
-  async function getUsers(){
+  async function getUsers() {
     const usersFromApi = await api.get('/users')
     setUsers(usersFromApi.data)
   }
 
-  async function createUser(){
+  async function createUser() {
     await api.post('/users', {
       name: inputName.current.value,
       email: inputEmail.current.value,
@@ -26,12 +27,49 @@ function Home() {
     })
 
     getUsers()
+    clearForm()
   }
 
-  async function deleteUser(id){
+  async function deleteUser(id) {
     await api.delete(`/users/${id}`)
+    getUsers()
+  }
+
+  async function editUser(id) {
+    const user = users.find(u => u.id === id)
+    inputName.current.value = user.name
+    inputEmail.current.value = user.email
+    inputAge.current.value = user.age
+    inputAddress.current.value = user.address
+    setEditingUserId(id)
+  }
+
+  async function updateUser() {
+    await api.put(`/users/${editingUserId}`, {
+      name: inputName.current.value,
+      email: inputEmail.current.value,
+      age: inputAge.current.value,
+      address: inputAddress.current.value
+    })
 
     getUsers()
+    clearForm()
+    setEditingUserId(null)
+  }
+
+  function clearForm() {
+    inputName.current.value = ''
+    inputEmail.current.value = ''
+    inputAge.current.value = ''
+    inputAddress.current.value = ''
+  }
+
+  function handleSubmit() {
+    if (editingUserId) {
+      updateUser()
+    } else {
+      createUser()
+    }
   }
 
   useEffect(() => {
@@ -39,30 +77,37 @@ function Home() {
   }, [])
 
   return (
-      <div className='container'>
-        <form>
-          <h1>Cadastro de usuários</h1>
-          <input id="inputName" placeholder="Nome" name="name" type="text" ref={inputName}></input>
-          <input id="inputEmail" placeholder="E-mail" name="email" type="email" ref={inputEmail}></input>
-          <input id="inputAge" placeholder="Idade" name="age" type="number" ref={inputAge}></input>
-          <input name="inputAddress" placeholder="Endereço" type="text" ref={inputAddress}></input>
-          <button id="buttonCreateUser" type="button" onClick={createUser}>Cadastrar</button>
-        </form>
+    <div className='container'>
+      <form>
+        <h1>Cadastro de usuários</h1>
+        <input id="inputName" placeholder="Nome" name="name" type="text" ref={inputName}></input>
+        <input id="inputEmail" placeholder="E-mail" name="email" type="email" ref={inputEmail}></input>
+        <input id="inputAge" placeholder="Idade" name="age" type="number" ref={inputAge}></input>
+        <input name="inputAddress" placeholder="Endereço" type="text" ref={inputAddress}></input>
+        <button id="buttonCreateUser" type="button" onClick={handleSubmit}>
+          {editingUserId ? 'Atualizar' : 'Cadastrar'}
+        </button>
+      </form>
 
-        {users.map((user) => (
-         <div key={user.id} className="card">
+      {users.map((user) => (
+        <div key={user.id} className="card">
           <div>
             <p>Nome: <span>{user.name}</span></p>
             <p>Email: <span>{user.email}</span></p>
             <p>Idade: <span>{user.age}</span></p>
             <p>Endereço: <span>{user.address}</span></p>
           </div>
-          <button onClick={() => deleteUser(user.id)}>
-            <img src={Trash} />
-          </button>
-         </div>
-        ))}
-      </div>
+          <span>
+            <button onClick={() => editUser(user.id)}>
+              <img src={Edit} />
+            </button>
+            <button onClick={() => deleteUser(user.id)}>
+              <img src={Trash} />
+            </button>
+          </span>
+        </div>
+      ))}
+    </div>
   )
 }
 
